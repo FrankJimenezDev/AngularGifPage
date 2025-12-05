8d65f3075878eff73ce78033075fe929130427de
Francisco Cohen <francisco.cohen@artekium.com>
2025-12-04 23:11:32 -0300

    [WIP] Seccion 8 del curso Angular Fernando Herrera

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -5,7 +5,7 @@ import { environment } from '@environments/environment';
 import { GiphyResponse } from '../interfaces/giphy.interface';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 const GIF_KEY = 'searchHistory';
 
@@ -22,10 +22,20 @@ export class GifService {
 
   trendingGifs = signal<Gif[]>([]);
   searchedGifs = signal<Gif[]>([]);
-
   trendingGifsLoading = signal<boolean>(false);
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
   searchHistoryKey = computed(() => Object.keys(this.searchHistory()));
+  private trendingPage = signal<number>(0);
+
+  trendingGifGroup = computed<Gif[][]>(() => {
+    const groups = [];
+
+    for (let i = 0; i < this.trendingGifs().length; i += 3) {
+      groups.push(this.trendingGifs().slice(i, i + 3));
+    }
+
+    return groups;
+  });
 
   constructor() {
     this.loadTrendingGifs();
@@ -37,16 +47,25 @@ export class GifService {
   });
 
   loadTrendingGifs() {
+
+    if (this.trendingGifsLoading()) return;
+    this.trendingGifsLoading.set(true);
+
     this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
       params: {
         api_key: environment.giphyApiKey,
         limit: '20',
+        offset: this.trendingPage() * 20,
       }
     })
       .pipe(finalize(() => this.trendingGifsLoading.set(false)))
       .subscribe(resp => {
         const gifs = GifMapper.mapGiphyItemsToGifs(resp.data);
-        this.trendingGifs.set(gifs);
+        this.trendingGifs.update(currentGifs => [
+          ...currentGifs,
+          ...gifs
+        ]);
+        this.trendingPage.update(page => page + 1);
       });
   }
 
